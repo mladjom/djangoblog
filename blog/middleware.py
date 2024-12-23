49a71fa65106b6234496c4cc3c663ecb65cb8b46
mladjom <mladenmilentijevic@gmail.com>
2024-12-23 13:31:33 +0100

    Dev

--- blog/middleware.py
+++
@@ -1,7 +0,0 @@
-class CurrentURLMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        request.current_url_name = request.resolver_match.url_name if request.resolver_match else None
-        return self.get_response(request)
